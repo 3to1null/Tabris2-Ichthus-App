50cbd7df33a1c3f8e49f2ac822b22180bf382e65
3to1null <nathanklumpenaar11@gmail.com>
2017-06-21 20:54:19 +0200

    New widget (toolbar)

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ setUIelements();
 
 let rootNavigationView = new NavigationView({
     left: 0, top: 0, right: 0, bottom: 0,
-    toolbarColor: colors.UI_bg
+    toolbarColor: colors.UI_bg,
+    id: 'rootNavigationView'
 }).appendTo(ui.contentView);
 
 new LoginPage().appendTo(rootNavigationView);
--- src/loginComponent/loginPage.js
+++ src/loginComponent/loginPage.js
@@ -1,8 +1,10 @@
 /**
  * Created by Nathan on 19-6-2017.
  */
-const {Button, Page, ui, Composite, TextView} = require('tabris');
+const {Page, ui, Composite, TextView} = require('tabris');
 const MaterialInput = require('../widgets/MaterialInput');
+const BigToolbar = require('../widgets/BigToolbar');
+const colors = require('../appSettings/colors');
 
 const pageTitle = 'Inloggen';
 const usernameInputLabel = 'Leerlingnummer';
@@ -12,21 +14,27 @@ module.exports = class LoginPage extends Page{
 
     constructor(properties) {
         super(Object.assign({title: pageTitle}, properties));
-        this._createLoginSegment()
+        this._rootNavigationView = ui.contentView.find('#rootNavigationView');
+        this._createLoginUI()
+        this._createInputSegment();
+    }
+
+    _createLoginUI(){
+        this._rootNavigationView.set('toolbarVisible', false);
+        new BigToolbar({top: 0, left: 0, right: 0, height: 180}, 'Inloggen', 'Gebruik je Ichthus account om in te loggen.').appendTo(this)
     }
 
     //loginSegment will be added to 'container'
-    _createLoginSegment(){
-        new TextView({
-            top: 15, left: 1, right: 0,
-            text: 'test',
-            font: '18px',
-            textColor: "#ff0000"
-        }).appendTo(this)
+    _createInputSegment(){
         this._usernameInputWidget = new MaterialInput(
-            {top: 'prev() 15', left: 30, right: 30},
+            {top: 'prev() 25', left: 36, right: 36},
             usernameInputLabel
-        ).appendTo(this)
+        ).appendTo(this);
+        this._passwordInputWidget = new MaterialInput(
+            {top: 'prev()', left: 36, right: 36},
+            passwordInputLabel,
+            'password'
+        ).appendTo(this);
     }
 
 }
