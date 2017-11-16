08c33c3cf65b23f9de8ca6f89314bc03bc2d0594
3to1null <nathanklumpenaar11@gmail.com>
2017-11-16 21:36:47 +0100

    Calendar listing finished
    Still need to add a create & detail page

--- src/calendarComponent/calendarPage.js
+++ src/calendarComponent/calendarPage.js
@@ -3,19 +3,34 @@
  */
 const {TextView, CollectionView, Composite, Page} = require('tabris');
 const IndeterminateProgressBar = require('../widgets/IndeterminateProgressBar');
+const FloatingActionButton = require('../widgets/FloatingActionButton');
 const colors = require('../appSettings/colors');
 const getCalendar = require('./getCalendar');
+const showToast = require('../globalFunctions/showToast');
 
 const initialPageTitle = 'Agenda';
 
 class AgendaPage extends Page {
   constructor(properties) {
     super(Object.assign({title: initialPageTitle, autoDispose: false}, properties));
+    this._progressBar = new IndeterminateProgressBar({left: 0, right: 0, top: 0, height: 4}).appendTo(this);
+
+    getCalendar(true).then((json)=>{
+      this._createCalendarList(json.Calendar);
+      this._updateCalendarList(this._calendarCollection);
+      this._createUI();
+      if(Boolean(json.Calendar[0].empty)){
+        showToast('Je agenda is leeg. Je kan een afspraak of opdracht toevoegen met het plusje.');
+        this._progressBar.top = 44;
+      }else{
+        this._progressBar.dispose();
+      }
+    }).catch((error) => {console.log(error)});
 
     this.on('appear', () => {
       this._logPage();
-      getCalendar().then((json)=>{this._createCalendarList(json.Calendar)})
     })
+
   }
 
   _logPage(){
@@ -23,31 +38,54 @@ class AgendaPage extends Page {
     firebase.Analytics.logEvent('agenda_opened', {screen: 'agendaScreen'});
   }
 
+  _createUI(){
+    new FloatingActionButton({
+      right: 16, bottom: 16, height: 56, width: 56,
+      color: colors.accent,
+      image: {src: "src/img/ic_add_white_36dp.png", width: 24, height: 24}
+    }).appendTo(this)
+  }
+
+  _updateCalendarList(collectionView){
+    getCalendar().then((json)=>{
+      let oldCalendarItems = this.items;
+      this.items = json.Calendar;
+      if(oldCalendarItems !== this.items){
+        collectionView.remove(0, oldCalendarItems.length);
+        collectionView.insert(0, this.items.length);
+        this.floatingSection.children()[0].text = this.items[0].text;
+      }
+      try{this._progressBar.dispose();}finally{}
+      collectionView.refreshIndicator = false;
+    }).catch((error)=>{console.log(error)})
+  }
+
   _createCalendarList(calendarItems){
-    const sectionHeight = 48;
+    this.sectionHeight = 48;
     const itemHeight = 78;
 
-    let scrollPosition = 0;
-    let items = calendarItems;
+    this.scrollPosition = 0;
+    this.items = calendarItems;
 
-    let floatingSection = createSectionView();
+    this.floatingSection = this._createSectionView();
 
-    floatingSection.children()[0].textColor = colors.white_bg;
-    floatingSection.background = colors.UI_bg;
-    floatingSection.elevation =  2;
-    floatingSection.height = sectionHeight;
-    floatingSection.children()[0].font = '18px';
-    floatingSection.children()[0].text = items[0].text;
+    this.floatingSection.children()[0].textColor = colors.white_bg;
+    this.floatingSection.background = colors.UI_bg;
+    this.floatingSection.elevation =  2;
+    this.floatingSection.height = this.sectionHeight - 4;
+    this.floatingSection.children()[0].font = '18px';
+    this.floatingSection.children()[0].text = this.items[0].text;
 
-    new CollectionView({
+    this._calendarCollection = new CollectionView({
       left: 0, top: 0, right: 0, bottom: 0,
-      itemCount: items.length,
-      cellType: index => items[index].type,
-      cellHeight: (index, type) => type === 'header' ? sectionHeight : itemHeight,
+      itemCount: this.items.length,
+      refreshEnabled: true,
+      cellType: index => this.items[index].type,
+      cellHeight: (index, type) => type === 'header' ? this.sectionHeight : itemHeight,
       createCell: (cellType) => {
         let cell = new Composite({backgroundColor: colors.white_bg});
         if(cellType === 'header'){
-          createSectionView().appendTo(cell);
+          this._createSectionView().appendTo(cell);
           return cell;
         }else{
           let title = new TextView({
@@ -74,84 +112,84 @@ class AgendaPage extends Page {
         }
       },
       updateCell: (cell, index) => {
-        console.log(items[index].startTimeParsed);
-        if(items[index].type === 'header'){
-          cell.children()[0].children()[0].text = items[index].text
+        if(this.items[index].type === 'header'){
+          cell.children()[0].children()[0].text = this.items[index].text;
+          if(Boolean(this.items[index].first)){
+            cell.children()[0].height = this.sectionHeight - 4
+          }
+
         }else{
-          if(Boolean(items[index]._divider)){
+          if(Boolean(this.items[index]._divider)){
             cell.children()[2].background = colors.divider_2
           }else{
             cell.children()[2].background = colors.transparant
           }
-          cell.children()[0].text = items[index].title;
-          cell.children()[1].text = items[index].descriptionPlainText;
-          cell.children()[3].text = items[index]._startTimeParsed;
+          cell.children()[0].text = this.items[index].title;
+          cell.children()[1].text = this.items[index].descriptionPlainText;
+          cell.children()[3].text = this.items[index]._startTimeParsed;
         }
       }
     }).on('scroll', ({target, deltaY})=> {
-      scrollPosition += deltaY;
+      this.scrollPosition += deltaY;
       let firstVisibleItem = target.firstVisibleIndex;
-      floatingSection.set({
-        text: getCurrentSection(firstVisibleItem).text,
-        transform: {translationY: getSectionTranslationY(firstVisibleItem)}
-      });
-      floatingSection.children()[0].text = getCurrentSection(firstVisibleItem).text;
+      this.floatingSection.children()[0].text = this._getCurrentSection(firstVisibleItem).text;
+    }).on('refresh', (eventObject) => {
+      this._updateCalendarList(eventObject.target);
     }).appendTo(this);
 
-    floatingSection.appendTo(this);
+    this.floatingSection.appendTo(this);
 
-    function createSectionView() {
-      let tmp = new tabris.Composite({
-        layoutData: {
-          top: 0,
-          right: 0,
-          height: sectionHeight,
-          left: 0
-        },
-        background: colors.white_grey_bg
-      });
-      new tabris.TextView({
-        top: 0, bottom: 0, left: 72, right: 0,
-        textColor: colors.black,
-        font: '15px',
-        alignment: 'left',
-        class: 'description'
-      }).appendTo(tmp);
-      return tmp
-    }
-
-    function getSectionTranslationY(firstVisibleItem) {
-      if (scrollPosition < 0) {
-        return -scrollPosition;
-      }
-      let nextSectionOffset = scrollPosition + sectionHeight - getNextSection(firstVisibleItem).top;
-      if (nextSectionOffset > 0) {
-        return -nextSectionOffset;
-      }
-      return 0;
-    }
-
-    function getNextSection(firstVisibleItem) {
-      for (let i = firstVisibleItem + 1; i < items.length; i++) {
-        let item = items[i];
-        if (item.type === 'header') {
-          return item;
-        }
-      }
-      return null;
-    }
-
-    function getCurrentSection(firstVisibleItem) {
-      for (let i = firstVisibleItem; i >= 0; i--) {
-        let item = items[i];
-        if (item.type === 'header') {
-          return item;
-        }
-      }
-      return null;
-    }
   }
 
+  _getCurrentSection(firstVisibleItem) {
+    for (let i = firstVisibleItem; i >= 0; i--) {
+      let item = this.items[i];
+      if (item.type === 'header') {
+        return item;
+      }
+    }
+    return null;
+  }
+  _getNextSection(firstVisibleItem) {
+    for (let i = firstVisibleItem + 1; i < this.items.length; i++) {
+      let item = this.items[i];
+      if (item.type === 'header') {
+        return item;
+      }
+    }
+    return null;
+  }
+
+  _getSectionTranslationY(firstVisibleItem) {
+    if (this.scrollPosition < 0) {
+      return -this.scrollPosition;
+    }
+    let nextSectionOffset = this.scrollPosition + this.sectionHeight - this._getNextSection(firstVisibleItem).top;
+    if (nextSectionOffset > 0) {
+      return -nextSectionOffset;
+    }
+    return 0;
+  }
+
+  _createSectionView() {
+    let tmp = new tabris.Composite({
+      layoutData: {
+        top: 0,
+        right: 0,
+        height: this.sectionHeight,
+        left: 0
+      },
+      background: colors.white_grey_bg
+    });
+    new tabris.TextView({
+      top: 0, bottom: 0, left: 72, right: 0,
+      textColor: colors.black,
+      font: '15px',
+      alignment: 'left',
+      class: 'description'
+    }).appendTo(tmp);
+    return tmp
+  }
 
 }
 
--- src/calendarComponent/getCalendar.js
+++ src/calendarComponent/getCalendar.js
@@ -3,10 +3,10 @@ const Request = require('../globalFunctions/Request');
 module.exports = function (fromOfflineStorage=false) {
   return new Promise((resolve, reject) => {
     let offlineAgenda = localStorage.getItem('calendar');
-    if(fromOfflineStorage === true && offlineAgenda !== null && offlineAgenda !== undefined){
+    if(fromOfflineStorage === true && offlineAgenda !== null && offlineAgenda !== undefined && offlineAgenda.length > 5){
       resolve(JSON.parse(offlineAgenda))
     }else{
-      new Request('getCalendar').get().then((response => {response.json().then((json) => {
+      new Request('calendar/get').get().then((response => {response.json().then((json) => {
         localStorage.setItem('calendar', JSON.stringify(json));
         resolve(json)
       })}))
