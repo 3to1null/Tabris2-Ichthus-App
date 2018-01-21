d60f696f41850c8b9aa7ab2703a566bb7216631a
3to1null <nathanklumpenaar11@gmail.com>
2018-01-21 20:57:52 +0100

    Release 2.1.0

--- src/globalFunctions/Request.js
+++ src/globalFunctions/Request.js
@@ -1,7 +1,8 @@
 /**
  * Created by Nathan on 23-6-2017.
  */
-const baseURL = "http://192.168.2.1:8000/ichthus/";
+//const baseURL = "http://192.168.2.1:8000/ichthus/";
+const baseURL = "https://api.fraignt.me/ichthus/";
 module.exports = class Request {
     constructor(urlPath, data, headers, baseURLSpecial, requestTimeout) {
         if (baseURLSpecial) {
